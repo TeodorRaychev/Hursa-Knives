c18910ac620a492f73b22421986f10893d04a952
TeodorRaychev <teodor.raychev.TR@gmail.com>
2023-12-06 20:46:23 +0200

    added products edit/delete functionality

--- src/main/java/com/hursa/hursaknives/model/dto/ProductBindingModel.java
+++ src/main/java/com/hursa/hursaknives/model/dto/ProductBindingModel.java
@@ -1,6 +1,7 @@
 package com.hursa.hursaknives.model.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record ProductBindingModel(
@@ -8,5 +9,5 @@ public record ProductBindingModel(
         @Size(min = 2, message = "Name must be at least 2 characters")
         String name,
     @Size(min = 5, message = "Description must be at least 5 characters") String description,
-    Double price,
+    @Positive(message = "Price must be positive number") Double price,
     String material) {}
--- src/main/java/com/hursa/hursaknives/service/ProductService.java
+++ src/main/java/com/hursa/hursaknives/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.hursa.hursaknives.service;
 
+import com.hursa.hursaknives.model.dto.ImageDTO;
 import com.hursa.hursaknives.model.dto.ProductBindingModel;
 import com.hursa.hursaknives.model.dto.ProductViewDTO;
-import com.hursa.hursaknives.model.entity.ImageEntity;
 import com.hursa.hursaknives.model.entity.ProductEntity;
 import com.hursa.hursaknives.repo.ImageRepository;
 import com.hursa.hursaknives.repo.ProductRepository;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
@@ -40,25 +41,66 @@ public class ProductService {
   }
 
   private ProductViewDTO map(ProductEntity e) {
-    List<ImageEntity> images = imageRepository.findByProductEntity_Id(e.getId());
+    List<ImageDTO> images = getImagesByProductId(e.getId());
     return new ProductViewDTO(
         e.getId(),
         e.getName(),
         e.getDescription(),
         e.getPrice() != null ? e.getPrice().doubleValue() : null,
         e.getMaterial(),
-        images.stream().map(ImageEntity::getUrl).collect(Collectors.toList()));
+        images);
   }
 
   public void deleteProduct(Long id) {
     productRepository.deleteById(id);
   }
 
-  public ProductViewDTO getProductById(Long id) {
+  public ProductViewDTO getProductViewDTOById(Long id) {
     ProductEntity productEntity =
         productRepository
             .findById(id)
             .orElseThrow(() -> new IllegalArgumentException("Product not found"));
     return map(productEntity);
+  }
+
+  public List<ImageDTO> getImagesByProductId(Long productId) {
+    return imageRepository.findByProductEntity_Id(productId).stream()
+        .map(i -> new ImageDTO(i.getId(), i.getUrl()))
+        .collect(Collectors.toList());
+  }
+
+  public void updateProduct(Long id, ProductBindingModel productBindingModel) {
+    if (productBindingModel.name() == null) {
+      throw new IllegalArgumentException("Product name cannot be null");
+    }
+    if (productBindingModel.description() == null) {
+      throw new IllegalArgumentException("Product description cannot be null");
+    }
+    ProductEntity productEntity =
+        productRepository
+            .findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("Product not found"));
+    boolean isChanged = false;
+    if (!productBindingModel.name().equals(productEntity.getName())) {
+      productEntity.setName(productBindingModel.name());
+      isChanged = true;
+    }
+    if (!productBindingModel.description().equals(productEntity.getDescription())) {
+      productEntity.setDescription(productBindingModel.description());
+      isChanged = true;
+    }
+    if (productBindingModel.price() != null && productEntity.getPrice() != null
+        && !productBindingModel.price().equals(productEntity.getPrice().doubleValue())) {
+      productEntity.setPrice(BigDecimal.valueOf(productBindingModel.price()));
+      isChanged = true;
+    }
+    if (productBindingModel.material() != null
+        && !productBindingModel.material().equals(productEntity.getMaterial())) {
+      productEntity.setMaterial(productBindingModel.material());
+      isChanged = true;
+    }
+    if (isChanged) {
+      productRepository.saveAndFlush(productEntity);
+    }
   }
 }
